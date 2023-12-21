616d8fcaa3b4f0002c09f9523cfcfef93aba1076
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2023-12-21 01:41:21 +0100

    Second story

---
+++ Assets/Scripts/UI/LinkButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LinkButton : MonoBehaviour
+{
+    public void OpenLink()
+    {
+        Application.OpenURL("https://github.com/HTML-Earth/Tskxekeng");
+    }
+}
